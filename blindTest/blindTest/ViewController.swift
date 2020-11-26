b37185b2608a873f9f2ce4c47ce3265f5d79588f
yannrelave <yann.relave@etu.univ-grenoble-alpes.fr>
2020-11-26 02:34:08 -0800

    Ajout bouton

--- blindTest/blindTest/ViewController.swift
+++ blindTest/blindTest/ViewController.swift
@@ -12,12 +12,16 @@ class ViewController: UIViewController {
     //Déclaration des objets
     @IBOutlet weak var titrelbl: UILabel!
     @IBOutlet weak var musicbtn: UIButton!
+    @IBOutlet weak var pubbtn: UIButton!
+    @IBOutlet weak var generiquebtn: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //Style du boutton
-        musicbtn.layer.cornerRadius = 3.0
+        musicbtn.layer.cornerRadius = 5.0
+        pubbtn.layer.cornerRadius = 5.0
+        generiquebtn.layer.cornerRadius = 5.0
     }
 
 
