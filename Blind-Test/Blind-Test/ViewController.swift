6e223e9e1c33afb6e291e24adf0042e138978d88
murienneh <hugomrn038@gmail.com>
2020-11-26 23:15:32 -0800

    Premier commit

---
+++ Blind-Test/Blind-Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Blind-Test
+//
+//  Created by Hugo on 26/11/2020.
+//  Copyright © 2020 Hugo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
