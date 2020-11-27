380571778e606f8b427105383db1bd66fe835db2
yannrelave <yann.relave@etu.univ-grenoble-alpes.fr>
2020-11-27 00:51:26 -0800

    Onglet musique

---
+++ Blind-Test/Blind-Test/MusiqueViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MusiqueViewController.swift
+//  Blind-Test
+//
+//  Created by yann on 27/11/2020.
+//  Copyright © 2020 Hugo. All rights reserved.
+//
+
+import UIKit
+
+class MusiqueViewController: UIViewController {
+    
+    //Déclaration des variables
+    @IBOutlet weak var lblTitleMusic: UILabel!
+    @IBOutlet weak var imgView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Blind-Test/Blind-Test/ViewController.swift
+++ Blind-Test/Blind-Test/ViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Déclaration des variables
+    @IBOutlet weak var titlelbl: UILabel!
+    @IBOutlet weak var btnMusic: UIButton!
+    @IBOutlet weak var btnGenerique: UIButton!
+    @IBOutlet weak var btnPublicite: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Arrondi des angles des boutons
+        btnMusic.layer.cornerRadius = 10.0
+        btnGenerique.layer.cornerRadius = 10.0
+        btnPublicite.layer.cornerRadius = 10.0
+    }
+    
+    //Lors d'un clic sur le bouton "Musique" va sur la page musique
+    @IBAction func clicMusiqueAction(){
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let musicViewController = storyboard.instantiateViewController(withIdentifier: "MusiqueViewController") as! MusiqueViewController
+        self.navigationController?.pushViewController(musicViewController, animated: true)
     }
 
 
