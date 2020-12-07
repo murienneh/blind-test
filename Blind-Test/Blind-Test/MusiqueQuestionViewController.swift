a3aa5e0af698a3623bb1afb2c39f808dd9074ffd
murienneh <hugomrn038@gmail.com>
2020-12-07 01:34:40 -0800

    musique

--- Blind-Test/Blind-Test/MusiqueQuestionViewController.swift
+++ Blind-Test/Blind-Test/MusiqueQuestionViewController.swift
@@ -6,9 +6,14 @@
 //  Copyright © 2020 Hugo. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class MusiqueQuestionViewController: UIViewController {
+    
+    @IBOutlet weak var btnPlay: UIButton!
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,15 +21,36 @@ class MusiqueQuestionViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func didTapButton(){
+        if let player = player, player.isPlaying{
+            btnPlay.setTitle("Play", for: .normal)
+            player.stop()
+        }
+        else{
+            btnPlay.setTitle("Stop", for: .normal)
+            let urlString = Bundle.main.path(forResource: "Coldplay - A Sky Full Of Stars", ofType: "mp3")
+            do {
+                try AVAudioSession.sharedInstance().setMode(.default)
+                try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+                
+                guard let urlString = urlString else {
+                    return
+                }
+                
+                player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: urlString))
+                
+                guard let player = player else {
+                    return
+                }
+                
+                player.play()
+                
+            }catch{
+                print("Problems")
+            }
+        }
+        
     }
-    */
+
 
 }
